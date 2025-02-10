4eb375e86a5e708e5fdd69dbbc8f70135ab5471c
joselopez177 <Pajileb@gmail.com>
2025-02-10 16:42:47 -0700

    initialcommit

--- django_ecommerce_mod5-master/ecommerce/settings.py
+++ django_ecommerce_mod5-master/ecommerce/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z+ksf@)0d^qojbh4rnp4b1to$hq&*tt(3bs$gf(3i267g$k9ln'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 #ALLOWED_HOSTS = ['dpg-cuhtqel2ng1s73dulvhg-a.oregon-postgres.render.com', '127.0.0.1', 'localhost']
 ALLOWED_HOSTS = ['https://jll-github-io.onrender.com']
